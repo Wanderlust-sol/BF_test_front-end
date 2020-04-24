b939252bc9212613511a3bf2b1c9804dfaad90ad
wanderlust_sol <taylorkwon92@gmail.com>
2020-04-24 19:32:36 +0900

    인트로 반응형 설정

--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { withRouter } from "react-router-dom";
 import styled from "styled-components";
 import figlet from "figlet";
 import standard from "figlet/importable-fonts/Standard.js";
 import Typing from "react-typing-animation";
 import Grain from "img/grain.png";
 
-const Intro = () => {
+const Intro = props => {
   const [mainText, setMainText] = useState("");
+  const [count, setCount] = useState(3);
+  const [finishType, setFinishType] = useState(false);
+  const [finishCount, setFinishCount] = useState(false);
+
   const getText = () => {
     figlet.parseFont("Standard", standard);
 
@@ -22,33 +27,92 @@ const Intro = () => {
     );
   };
 
-  useEffect(() => getText(), []);
+  const countingNum = () => {
+    setCount(count - 1);
 
+    if (count === 0) {
+      setFinishCount(true);
+    }
+  };
+
+  const startClick = () => {
+    props.history.push("/");
+  };
+
+  useEffect(() => getText(), []);
   return (
     <>
       <IntroWrapper>
         <GrainBg></GrainBg>
-        <Typing speed={50}>
-          <TypedText>
-            BF 테스트에 오신걸 환영합니다.
-            <br />
-            이 테스트를 통해 자신의 성향이 프론트엔드와 백엔드중 어디에 더
-            가까운지 알아보세요.
-            <br />
-            <br />
-            *경고*
-            <br />이 테스트는 그저 테스트일 뿐이므로 참고만 해주시기 바랍니다.
-          </TypedText>
-        </Typing>
-        <MainText>
-          <code>{mainText}</code>
-        </MainText>
+        {!finishCount && (
+          <Typing
+            speed={40}
+            onFinishedTyping={() => {
+              setFinishType(true);
+            }}
+          >
+            <TypedText finishType={finishType}>
+              옛날 옛적에, 코딩의 코자도 모르던 사람들이 위코드를 통해 처음
+              코딩을 배우게 되었습니다.
+              <br />
+              <br />
+              그런데 시련은 생각보다 빨리 찾아왔습니다.
+              <br />
+              백엔드와 프론트엔드를 선택하는 갈림길에 서고야 만 것이죠...
+              <br />
+              <br />
+              서로를 부둥켜 안고 눈물 흘렸던 이들은 길고 긴 고민 끝에 한 가지
+              결심을 하고 각자 길을 떠납니다.
+              <br />
+              <br />
+              휼륭한 개발자가 되어 다시 만나서 이 어려움을 해결할 전설의
+              테스트를 만들겠다는 결심이었죠.
+              <br />
+              <br />
+              .<br />
+              .<br />
+              .<br />
+              <br />
+              2020년 4월, 훌륭한 프론트엔드와 백엔드 개발자로 성장한 이들이
+              결심을 이루러 모였습니다.
+              <br />
+              <br />
+              기대되지 않으신가요?
+              <br />
+              프론트엔드와 백엔드 중 당신의 성향에 맞는 포지션을 찾아주는 전설의
+              심리테스트!
+              <br />
+              <br />
+              지금 바로 BF 테스트의 세계로 여러분을 초대합니다!
+            </TypedText>
+          </Typing>
+        )}
+        {finishType && (
+          <Typing
+            speed={300}
+            loop={count === -1 ? false : true}
+            onFinishedTyping={() => {
+              countingNum();
+            }}
+          >
+            <TypedNum>{count}</TypedNum>
+            <Typing.Backspace count={1} delay={500} />
+          </Typing>
+        )}
+        {finishCount && (
+          <MainWrapper>
+            <MainText>
+              <code>{mainText}</code>
+            </MainText>
+            <StartBox onClick={startClick}>START</StartBox>
+          </MainWrapper>
+        )}
       </IntroWrapper>
     </>
   );
 };
 
-export default Intro;
+export default withRouter(Intro);
 
 const IntroWrapper = styled.div`
   background-color: black;
@@ -114,27 +178,56 @@ const GrainBg = styled.div`
 
 const TypedText = styled.p`
   color: #60bb5e;
-  font-size: 18px;
-  position: absolute;
-  /* left: 370px; */
-  /* top: 180px; */
-  top: 0;
-  z-index: 100;
+  font-size: 20px;
   line-height: 30px;
+  position: absolute;
+  left: 20px;
+  top: 20px;
+  z-index: 100;
+
+  @media only screen and (max-width: 415px) {
+    display: ${props => props.finishType && "none"};
+    font-size: 15px;
+    width: 92%;
+  }
+`;
+
+const TypedNum = styled.span`
+  color: #60bb5e;
+  font-size: 23px;
+  line-height: 30px;
+  position: absolute;
+  left: 520px;
+  top: 560px;
+  z-index: 100;
+
+  @media only screen and (max-width: 415px) {
+    font-size: 145px;
+    left: 42%;
+    top: 46%;
+  }
+`;
+
+const MainWrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
   display: flex;
+  flex-direction: column;
   align-items: center;
 `;
 
 const MainText = styled.pre`
   color: #60bb5e;
-  font-size: 25px;
-  position: absolute;
-  left: 50%;
-  top: 350px;
-  transform: translateX(-50%);
-  z-index: 100;
+  font-size: 30px;
   font-family: monospace;
   animation: blinker 0.4s infinite;
+
+  @media only screen and (max-width: 415px) {
+    font-size: 1.2rem;
+  }
 
   @keyframes blinker {
     from {
@@ -142,6 +235,35 @@ const MainText = styled.pre`
     }
     to {
       opacity: 1;
+    }
+  }
+`;
+
+const StartBox = styled.div`
+  width: 150px;
+  height: 45px;
+  color: #60bb5e;
+  font-size: 35px;
+  border: 2px solid #60bb5e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+  cursor: pointer;
+  animation: startblinker 0.4s infinite;
+
+  @keyframes startblinker {
+    0% {
+      color: #60bb5e;
+      border: 2px solid #60bb5e;
+    }
+    50% {
+      color: black;
+      background-color: #60bb5e;
+    }
+    100% {
+      color: #60bb5e;
+      border: 2px solid #60bb5e;
     }
   }
 `;
